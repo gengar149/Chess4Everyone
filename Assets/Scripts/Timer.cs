2474d77aed9bbe359ee129a7a1ae95ea2c53ea99
GaetanJago <gaetanjago@gmail.com>
2021-02-12 16:05:54 +0100

    add timer for black and white

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    private TMP_Text clock;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clock = GetComponent<TMP_Text>();
+        timerIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out !");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        clock.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
